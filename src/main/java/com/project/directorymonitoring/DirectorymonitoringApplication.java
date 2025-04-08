53cddb1e2816c102783955ee99f555caeb85cf6d
RoyArunim <royarunimb12@gmail.com>
2025-04-08 15:13:38 +0530

    Setting up project

---
+++ src/main/java/com/project/directorymonitoring/DirectorymonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.project.directorymonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DirectorymonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DirectorymonitoringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/directorymonitoring/DirectorymonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.directorymonitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DirectorymonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
